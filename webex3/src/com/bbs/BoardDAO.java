6f5e9f9d1d62d8be62a80c0a5941bb75958bc7ff
user2 <user2@user2-PC>
2017-01-25 14:47:42 +0900

    1

---
+++ webex3/src/com/bbs/BoardDAO.java
@@ -0,0 +1,26 @@
+package com.bbs;
+
+import java.util.List;
+
+public interface BoardDAO {
+	public int insertBoard(BoardDTO dto);
+	public int dataCount();
+	public int dataCount(String searchKey, String searchValue);
+	public List<BoardDTO> listBoard(int start, int end);
+	public List<BoardDTO> listBoard(int start, int end, String searchKey, String searchValue);	
+	public int updateHitCount(int num);
+	public BoardDTO readBoard(int num);
+	public BoardDTO preReadBoard(int num, String searchKey, String searchValue);
+    public BoardDTO nextReadBoard(int num, String searchKey, String searchValue);	
+	public int updateBoard(BoardDTO dto);
+	public int deleteBoard(int num);	
+	
+	public int insertReply(ReplyDTO dto);
+	public int dataCountReply(int num);
+	public List<ReplyDTO> listReply(int num, int start, int end);
+	public int deleteReply(int replyNum);
+	
+	public List<ReplyDTO> listReplyAnswer(int answer);
+	public int dataCountReplyAnswer(int answer);
+	public int deleteReplyAnswer(int replyNum);
+}
